7a6ebf4bce7c65a6c736ba4e3516031e56dfb4ea
Cale Winebrenner <cale@appl.red>
2020-10-28 15:46:28 -0500

    Initial commit.

---
+++ src/main/java/red/appl/esi/EsiApiApplication.java
@@ -0,0 +1,13 @@
+package red.appl.esi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsiApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/red/appl/ssrsrest/EsiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package red.appl.ssrsrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
